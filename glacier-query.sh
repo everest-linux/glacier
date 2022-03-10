1ffb79e849aeb8194c1681600bafcbbad2de3bd4
everest <liamwaldron7009@protonmail.com>
2022-03-10 17:29:47 +0000

    Update glacier-query.sh

--- glacier-query.sh
+++ glacier-query.sh
@@ -4,3 +4,7 @@
 
 echo "[ ? ] Enter package name:" && read input
 cat /etc/glacier/pkginfo/$input-pkginfo.json
+if [ "$?" != "0" ]; then
+    echo "[ X ] Package $input not found!" 1>&2
+    exit 1
+fi
