0311d859c9fa6fe7b62b8582fd6c5cc2c1f98f66
everest <liamwaldron7009@protonmail.com>
2022-03-10 17:09:10 +0000

    Update UNINSTALL-GLACIER.sh

--- UNINSTALL-GLACIER.sh
+++ UNINSTALL-GLACIER.sh
@@ -7,9 +7,14 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
 fi
 
 echo "[ ! ] It is recommended to uninstall all Glacier packages before uninstalling Glacier."
-rm -rf /etc/glacier
-rm /bin/glacier-install
-rm /bin/glacier-update
-rm /bin/glacier-remove
-rm /bin/glacier-query
-rm /bin/glacier-help
+echo "[ ! ] This will prevent untracked packages from remaining on your system and possibly creating a security risk."
+echo "[ ! ] To see which packages are installed on your system, run the following command:"
+echo "[ ! ] ls /etc/glacier/pkginfo"
+while true; do
+    read -p "[ ? ] Would you like to uninstall Glacier?" yn
+    case $yn in
+        [Yy]* ) rm -rf /etc/glacier && rm /bin/glacier-install && rm /bin/glacier-update && rm /bin/glacier-remove && rm /bin/glacier-query && rm /bin/glacier-help; break;;
+        [Nn]* ) exit;;
+        * ) echo "[ ! ] You must answer either yes or no.";;
+    esac
+done
