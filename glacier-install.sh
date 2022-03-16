544f1e00f1d8adfe91299845817409cd5557e17d
everest <liamwaldron7009@protonmail.com>
2022-03-16 23:18:09 +0000

    Update glacier-install.sh

--- glacier-install.sh
+++ glacier-install.sh
@@ -16,7 +16,7 @@ if [ "$?" != "0" ]; then
     exit 1
 fi
 echo "[ i ] Unpacking $input.tar.gz..."
-mkdir $input
+mkdir $input && cd $input
 tar -xvf $input.tar.gz
 if [ "$?" != "0" ]; then
     echo "[ X ] Could not unpack $input.tar.gz" 1>&2
