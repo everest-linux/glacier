2f0161baacf6b5f2328aacdb0a5cc49bd8637e8a
everest <liamwaldron7009@protonmail.com>
2022-03-24 13:38:32 +0000

    Update INSTALL-GLACIER.sh

--- INSTALL-GLACIER.sh
+++ INSTALL-GLACIER.sh
@@ -13,13 +13,16 @@ chmod +x glacier-install.sh
 chmod +x glacier-remove.sh
 chmod +x glacier-update.sh
 chmod +x glacier-query.sh
+chmod +x glacier-help.sh
 echo "[ i ] Installing glacier..."
 cp glacier-install.sh glacier-install
 cp glacier-remove.sh glacier-remove
 cp glacier-update.sh glacier-update
 cp glacier-query.sh glacier-query
+cp glacier-help.sh glacier-help
 mv glacier-install /bin
 mv glacier-remove /bin
 mv glacier-update /bin
 mv glacier-query /bin
+mv glacier-help /bin
 echo "[ i ] Glacier has finished installing successfully."
