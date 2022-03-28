1b7f0699e305492fc560cb9d49bd1faa1cad43fa
everest <liamwaldron7009@protonmail.com>
2022-03-28 21:03:16 +0000

    Create glacier-cache.sh

---
+++ glacier-cache.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# glacier-cache
+# Script used to download a package and cache it
+
+# Require the script to be run as root
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "[ X ] Please run Glacier as root."
+    exit
+fi
+
+echo "[ ? ] Enter package name:" && read input
+echo "[ i ] Caching $input.tar.gz"
+wget https://github.com/everest-linux/glacier-pkgs/raw/main/pkgs/$input.tar.gz
+if [ "$?" != "0" ]; then
+    echo "[ X ] Could not get archive for $input." 1>&2
+    exit 1
+fi
+mv $input.tar.gz /var/cache/glacier
+echo "[ i ] Operation completed.
