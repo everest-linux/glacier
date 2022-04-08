ecae5c90ad1205cbf5ca185b355d9b358d96411c
everest <liamwaldron7009@protonmail.com>
2022-04-08 16:51:33 +0000

    Create WGET-INSTALL.sh

---
+++ WGET-INSTALL.sh
@@ -0,0 +1,7 @@
+#!/bin.sh
+
+mkdir ../glacier-tmp
+wget https://github.com/everest-linux/glacier/blob/main/glacier-install.sh
+wget https://github.com/everest-linux/glacier/blob/main/glacier-update.sh
+wget https://github.com/everest-linux/glacier/blob/main/glacier-remove.sh
+wget https://github.com/everest-linux/glacier/blob/main/glacier-query.sh
