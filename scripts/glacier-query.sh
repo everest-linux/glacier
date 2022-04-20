f65684c0fd055ee09d8de4b8b96e944a4e1c6f5e
everest <liamwaldron7009@protonmail.com>
2022-04-20 19:44:06 +0000

    Update glacier-query.sh

--- scripts/glacier-query.sh
+++ scripts/glacier-query.sh
@@ -2,10 +2,10 @@
 # glacier-query
 # Script used to query packages on the system
 
-echo "[ ? ] Enter package name:" && read input
+printf "\033[1;34m [ ? ] \033[m Enter package name: " && read input
 cat /etc/glacier/pkginfo/$input-pkginfo.json
 cat /var/log/glacier/$input.timestamp
 if [ "$?" != "0" ]; then
-    echo "[ X ] Package $input not found!" 1>&2
+    printf "\033[1;31m [ $error ] \033[m Package not found. " 1>&2
     exit 1
 fi
