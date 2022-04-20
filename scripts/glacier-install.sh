6c0a7ded217679a9f7fcb62facb29e3ccd7ec102
everest <liamwaldron7009@protonmail.com>
2022-04-20 19:00:36 +0000

    Update glacier-install.sh

--- scripts/glacier-install.sh
+++ scripts/glacier-install.sh
@@ -17,31 +17,31 @@ export question="\x3F"
 
 # Require the script to be run as root
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-    echo "\033[1;31m[ $error ]\033[m Please run Glacier as root."
+    echo "\033[1;31m [ $error ] \033[m Please run Glacier as root."
     exit
 fi
 
-echo "\033[1;34m[ ? ]\033[m Enter package name:" && read input
-echo "\033[1;34m[ i ]\033[m Installing $input.tar.gz"
+echo "\033[1;34m [ ? ] \033[m Enter package name:" && read input
+echo "\033[1;34m [ i ] \033[m Installing $input.tar.gz"
 echo "Checking world..." && wget https://github.com/everest-linux/glacier-pkgs/raw/main/world/$input.tar.gz
 if [ "$?" != "0" ]; then
-    echo "\033[1;31m[ $error ]\033[m Package not found." 1>&2
+    echo "\033[1;31m [ $error ] \033[m Package not found." 1>&2
     exit 1
 fi
-echo "\033[1;34m[ i ]\033[m Unpacking $input.tar.gz..."
+echo "\033[1;34m [ i ] \033[m Unpacking $input.tar.gz..."
 mkdir -v $input && mv -v $input.tar.gz $input && cd $input
 tar -xvf $input.tar.gz
 if [ "$?" != "0" ]; then
-    echo "\033[1;31m[ $error ]\033[m Could not unpack $input.tar.gz" 1>&2
+    echo "\033[1;31m [ $error ] \033[m Could not unpack $input.tar.gz" 1>&2
     exit 1
 fi
 chmod +x INSTALL.sh
 chmod +x $input.ts.sh
-echo "\033[1;34m[ i ]\033[m Executing installation instructions..."
+echo "\033[1;34m [ i ] \033[m Executing installation instructions..."
 ./INSTALL.sh # Actually executes installation script
 ./$input.ts.sh
-echo "\033[1;34m[ i ]\033[m Cleaning up..." # Status message
+echo "\033[1;34m [ i ] \033[m Cleaning up..." # Status message
 mv -v $input-pkginfo.json /etc/glacier/pkginfo
 cd ..
 rm -rvf $input
-echo "\033[1;32m[ $check ]\033[m Operation completed."
+echo "\033[1;32m [ $check ] \033[m Operation completed."
