c83c6a6cbcdbdd9aa0e7a5c1de360a450f39b603
everest <liamwaldron7009@protonmail.com>
2022-05-02 16:43:14 +0000

    Update glacier-cache-clear.sh

--- scripts/glacier-cache-clear.sh
+++ scripts/glacier-cache-clear.sh
@@ -2,6 +2,10 @@
 # glacier-cache-clear
 # Script used to clear Glacier's cache
 
+# Glacier preloading
+
+source /etc/glacier.conf
+
 # Require the script to be run as root
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     printf "\033[1;31m [ $error ] \033[m Please run Glacier as root."
