0e616b6771e33ad2b24cdcc6a3eb830d1bcaeb63
everest <liamwaldron7009@protonmail.com>
2022-05-03 15:37:28 +0000

    Update glacier-install.sh

--- scripts/glacier-install.sh
+++ scripts/glacier-install.sh
@@ -26,13 +26,13 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
 fi
 
 printf "\033[1;34m [ ? ] \033[m Enter package name: " && read input
-printf "\033[1;34m [ i ] \033[m Installing $input.tar.gz... "
-printf "Checking world... " && wget https://github.com/everest-linux/glacier-pkgs/raw/main/world/$input.tar.gz
+printf "\033[1;34m [ i ] \033[m Installing $input.tar.gz...\n"
+printf "Checking world... " && wget https://github.com/everest-linux/glacier-pkgs/raw/main/world/$input.tar.gz -q --show-progress
 if [ "$?" != "0" ]; then
     printf "\033[1;31m [ \xE2\x9C\x95 ] \033[m Package not found. " 1>&2
     exit 1
 fi
-printf "\033[1;34m [ i ] \033[m Unpacking $input.tar.gz... "
+printf "\033[1;34m [ i ] \033[m Unpacking $input.tar.gz...\n"
 mkdir $input && mv $input.tar.gz $input && cd $input
 tar -xf $input.tar.gz
 if [ "$?" != "0" ]; then
@@ -41,11 +41,11 @@ if [ "$?" != "0" ]; then
 fi
 chmod +x INSTALL.sh
 chmod +x $input.ts.sh
-printf "\033[1;34m [ i ] \033[m Executing installation instructions... "
+printf "\033[1;34m [ i ] \033[m Executing installation instructions...\n"
 ./INSTALL.sh # Actually executes installation script
 ./$input.ts.sh
-printf "\033[1;34m [ i ] \033[m Cleaning up... " # Status message
+printf "\033[1;34m [ i ] \033[m Cleaning up...\n" # Status message
 mv $input-pkginfo.json /etc/glacier/pkginfo
 cd ..
 rm -rf $input
-printf "\033[1;32m [ \xE2\x9C\x93 ] \033[m Operation completed."
+printf "\033[1;32m [ \xE2\x9C\x93 ] \033[m Operation completed.\n"
