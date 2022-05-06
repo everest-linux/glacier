9e9f705fa396432be548b7e765e8cd051eae189a
everest <liamwaldron7009@protonmail.com>
2022-05-06 14:02:35 +0000

    Update glacier-cache.sh

--- scripts/glacier-cache.sh
+++ scripts/glacier-cache.sh
@@ -14,7 +14,7 @@ fi
 
 printf "\033[1;34m [ ? ] \033[m Enter package name: " && read input
 printf "\033[1;34m [ i ] \033[m Caching $input.tar.gz...\n"
-printf "Checking world... " && wget $GREPO1/$input.tar.gz -q --show-progress || wget $GREPO2/$input.tar.gz -q --show-progress || wget $GREPO3/$input.tar.gz -q --show-progress || wget $GREPO4/$input.tar.gz -q --show-progress || wget $GREPO5/$input.tar.gz -q --show-progress || wget $GREPO6/$input.tar.gz -q --show-progress || wget $GREPO7/$input.tar.gz -q --show-progress || wget $GREPO8/$input.tar.gz -q --show-progress
+printf "\033[1;34m [ i ] \033[m Checking databases... " && wget $GREPO1/$input.tar.gz -q --show-progress || wget $GREPO2/$input.tar.gz -q --show-progress || wget $GREPO3/$input.tar.gz -q --show-progress || wget $GREPO4/$input.tar.gz -q --show-progress || wget $GREPO5/$input.tar.gz -q --show-progress || wget $GREPO6/$input.tar.gz -q --show-progress || wget $GREPO7/$input.tar.gz -q --show-progress || wget $GREPO8/$input.tar.gz -q --show-progress
 if [ "$?" != "0" ]; then
     printf "\033[1;31m [ $error ] \033[m Package not found. " 1>&2
     exit 1
