a82e25c9809b700b7f2cc89505e096f44927a1a5
everest <liamwaldron7009@protonmail.com>
2022-05-06 11:51:19 +0000

    Update glacier-help.sh

--- scripts/glacier-help.sh
+++ scripts/glacier-help.sh
@@ -16,5 +16,4 @@ echo "IMPORTANT REMINDER - Glacier will ask you which package to install, glacie
 echo ""
 echo "Glacier - Copyright (C) 2022  Everest Linux"
 echo "This program comes with ABSOLUTELY NO WARRANTY; for details see section 15 of the GNU GPL v3."
-echo "This is free software, and you are welcome to redistribute it"
-echo "under certain conditions; type `show c' for details."
+echo "This is free software, and you are welcome to redistribute it under certain conditions; see the GNU GPL v3 for details."
