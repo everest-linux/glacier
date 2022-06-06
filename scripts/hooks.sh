014a8c82a999856e880512df4c625b3cf91d3a02
everest <liamwaldron7009@protonmail.com>
2022-06-06 14:59:08 +0000

    Update hooks.sh

--- scripts/hooks.sh
+++ scripts/hooks.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # /etc/glacier/hooks.sh
 # Hooks that will be run while calling Glacier
+# DO NOT DELETE THE 2 HOOKS BELOW
 
 source /etc/glacier.conf
 source /etc/make.conf
+
+# Custom hooks can be put below
